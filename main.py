279fafc570230eb4e61c4b8c80a058dd78eba36b
Anna-Surma <anna.surma2000@gmail.com>
2022-06-23 21:05:42 +0200

    Changes in numbers of row and dictionary save.

--- main.py
+++ main.py
@@ -6,80 +6,47 @@ import bs4 as bs
 import pandas as pd
 import csv
 
-# TODO Jakość kodu i raport (3/4)
-
-
-# TODO Skuteczność klasyfikacji (0/4)
-# TODO [0.00, 0.50) - 0.0
-# TODO [0.50, 0.55) - 0.5
-# TODO [0.55, 0.60) - 1.0
-# TODO [0.60, 0.65) - 1.5
-# TODO [0.65, 0.70) - 2.0
-# TODO [0.70, 0.75) - 2.5
-# TODO [0.75, 0.80) - 3.0
-# TODO [0.80, 0.85) - 3.5
-# TODO [0.85, 1.00) - 4.0
-
-# stderr:
-# Traceback (most recent call last):
-#   File "main.py", line 238, in <module>
-#     desc_input_tab = extract_input()
-#   File "main.py", line 210, in extract_input
-#     descriptor = bow.compute(to_gray(sightPart), sift.detect(to_gray(sightPart)))
-#   File "main.py", line 95, in to_gray
-#     gray = cv2.cvtColor(color_img, cv2.COLOR_BGR2GRAY)
-# cv2.error: OpenCV(4.5.4) /tmp/pip-req-build-th1mncc2/opencv/modules/imgproc/src/color.cpp:182: error: (-215:Assertion failed) !_src.empty() in function 'cvtColor'
-
-# TODO Zla sciezka.
 path = os.getcwd()
 upperPath = os.path.abspath(os.path.join(path, os.pardir))
-
 train_annotations_path = upperPath + "/train/annotations/"
 train_images_path = upperPath + "/train/images/"
 train_annotations_files = os.listdir(train_annotations_path)
 train_images_files = os.listdir(train_images_path)
-
 test_images_path = upperPath + "/test/images/"
 test_images_files = os.listdir(test_images_path)
 
-
 def create_train_csv():
     data_xml = []
-
     for image in train_annotations_files:
         annot_xml_path = train_annotations_path + image
         soup = bs.BeautifulSoup(open(annot_xml_path, "r").read(), 'xml')
-
         image_size = soup.find('size')
         image_shape = [image_size.find('width').text, image_size.find('height').text]
-
         for object in soup.find_all('object'):
             bndbox = object.find('bndbox')
             xmin = int(bndbox.find('xmin').text)
-            ymin = int(bndbox.find('ymin').text)
             xmax = int(bndbox.find('xmax').text)
+            ymin = int(bndbox.find('ymin').text)
             ymax = int(bndbox.find('ymax').text)
 
             x_size = int(xmax - xmin)
             y_size = int(ymax - ymin)
 
-            if((x_size > int(image_shape[0]) / 10) or (y_size > int(image_shape[1]) / 10)):
+            if ((x_size < int(image_shape[0]) / 10) or (y_size < int(image_shape[1]) / 10)):
+                continue
+            else:
                 name = object.find('name').text
                 filename = soup.find('filename').text
-
                 if(name == 'speedlimit'):
                     type = 1
                 else:
                     type = 2
-
                 if (name != 'speedlimit'):
                     name = 'other'
-
-                xml_val = [filename, name, type, xmin, ymin, xmax, ymax]
-
+                xml_val = [filename, name, type, xmin,  xmax, ymin, ymax]
                 # TODO Nie łatwiej to wrzucić do jakiejś struktury, np. słowinka, zamiast zapisywać na dysku w innym formacie?
                 data_xml.append(xml_val)
-                col_names = ['filename', 'name', 'type', 'x_min', 'y_min', 'x_max', 'y_max']
+                col_names = ['filename', 'name', 'type', 'x_min', 'x_max', 'y_min',  'y_max']
                 df = pd.DataFrame(data_xml, columns=col_names)
                 df.to_csv(path + '/X_train.csv', index=False)
 
@@ -105,18 +72,18 @@ def BOW():
                 delete_first = 1
             else:
                 image = cv2.imread(train_images_path + row[0])
-                image_part = image[int(row[4]): int(row[6]), int(row[3]): int(row[5])]
+                image_part = image[int(row[5]): int(row[6]), int(row[3]): int(row[4])]
                 kp, dsc = gen_sift_features(to_gray(image_part))
 
                 if dsc is not None:
                     bow.add(dsc)
     dictionary = bow.cluster()
-    return dictionary
+    np.save('dictionary.npy', dictionary)
 
 def prepere_bow(sift):
     flann = cv2.FlannBasedMatcher_create()
     bow = cv2.BOWImgDescriptorExtractor(sift, flann)
-    dictionary = BOW()
+    dictionary = np.load('dictionary.npy')
     bow.setVocabulary(dictionary)
     return bow
 
@@ -133,7 +100,7 @@ def extract_train():
                 delete_first = 1
             else:
                 image = cv2.imread(train_images_path + row[0])
-                sightPart = image[int(row[4]): int(row[6]), int(row[3]): int(row[5])]
+                sightPart = image[int(row[5]): int(row[6]), int(row[3]): int(row[4])]
                 descriptor = bow.compute(to_gray(sightPart), sift.detect(to_gray(sightPart)))
 
                 if descriptor is not None:
@@ -172,12 +139,11 @@ def create_input_csv():
         filename = str(input())
         bndbox_nr = int(input())
         for j in range(bndbox_nr):
-            # TODO Dane są w formacie xmin, xmax, ymin, ymax.
             xmin, xmax, ymin, ymax = input().split()
             l = [filename, xmin, xmax, ymin, ymax]
             data_input.append(l)
 
-    col_names = ['filename', 'x_min', 'x_max', 'x_max', 'y_max']
+    col_names = ['filename', 'x_min', 'x_max', 'y_min', 'y_max']
     df = pd.DataFrame(data_input, columns=col_names)
     df.to_csv(path + '/X_test.csv', index=False)
 
@@ -194,7 +160,7 @@ def extract_input():
                 delete_first = 1
             else:
                 image = cv2.imread(test_images_path + row[0])
-                sightPart = image[int(row[2]): int(row[4]), int(row[1]): int(row[3])]
+                sightPart = image[int(row[3]): int(row[4]), int(row[1]): int(row[2])]
                 descriptor = bow.compute(to_gray(sightPart), sift.detect(to_gray(sightPart)))
 
                 if descriptor is not None:
@@ -217,6 +183,7 @@ if __name__ == '__main__':
     x = input()
 
     create_train_csv()
+    BOW()
     train_desc_tab = extract_train()
     train_data = train(train_desc_tab)
 
